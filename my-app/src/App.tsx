fbeb5882b8aa1263a312c4835df70009fe9e771d
Darvan Ceyhan <115955030+darwanceyhan@users.noreply.github.com>
2023-04-30 15:45:18 +0300

    update navbar and change the style

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,22 +1,23 @@
 import "./index.css";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+
 function App() {
   return (
-    <div className="container">
-      <div className="flex flex-row">
-        <div className="basis-1/4">Users</div>
+    <div className="flex flex-row items-center h-20 w-full border-b-2">
+      <div className="basis-1/4">Users</div>
+      <div className="basis-1/2 h-full flex items-center">
+        <Stack spacing={8} direction="row">
+          <button className="navbarFilterItems">All Users</button>
+          <button className="navbarFilterItems">Contributor</button>
+          <button className="navbarFilterItems">Author</button>
+          <button className="navbarFilterItems">Adminstrator</button>
+          <button className="navbarFilterItems">Subscriber</button>
+        </Stack>
+      </div>
+      <div className="basis-1/4 flex flex-row">
+        <div className="basis-1/2"></div>
         <div className="basis-1/2">
-          <Stack spacing={2} direction="row">
-            <Button variant="text">All Users</Button>
-            <Button variant="text">Contributor</Button>
-            <Button variant="text">Author</Button>
-            <Button variant="text">Adminstrator</Button>
-            <Button variant="text">Subscriber</Button>
-          </Stack>
-        </div>
-        <div className="basis-1/4 grid grid-cols-2">
-          <div></div>
           <Button variant="contained">Add New User</Button>
         </div>
       </div>
