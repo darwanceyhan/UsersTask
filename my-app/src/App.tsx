8d1e1b2590a0c0884b213044522c50ef8a79d17f
Darvan Ceyhan <115955030+darwanceyhan@users.noreply.github.com>
2023-04-30 14:24:23 +0300

    update navbar and change the style

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,13 +8,16 @@ function App() {
         <div className="basis-1/4">Users</div>
         <div className="basis-1/2">
           <Stack spacing={2} direction="row">
-            <Button variant="text">Text</Button>
-            <Button variant="text">Contained</Button>
-            <Button variant="text">Outlined</Button>
+            <Button variant="text">All Users</Button>
+            <Button variant="text">Contributor</Button>
+            <Button variant="text">Author</Button>
+            <Button variant="text">Adminstrator</Button>
+            <Button variant="text">Subscriber</Button>
           </Stack>
         </div>
-        <div className="basis-1/4">
-          <Button variant="contained">Add Neww User</Button>
+        <div className="basis-1/4 grid grid-cols-2">
+          <div></div>
+          <Button variant="contained">Add New User</Button>
         </div>
       </div>
     </div>
