57d2eabb3cbc4d1b9c7d5f3685deddfc3c3e9862
Darvan Ceyhan <115955030+darwanceyhan@users.noreply.github.com>
2023-05-01 03:03:20 +0300

    create data.jsx file and UserInterface.ts file

---
+++ my-app/src/data/data.jsx
@@ -0,0 +1,18 @@
+const UserData = [
+  {
+    id: 1,
+    name: "zachary gonzalez",
+    username: "zachary-gonzalez",
+    email: "zachary-gonzalez@gmail.com",
+    role: "Editor",
+  },
+  {
+    id: 2,
+    name: "amanda montogomery",
+    username: "amanda-montogomery",
+    email: "montogomery@ya.com",
+    role: "Subscriber",
+  },
+];
+
+export default UserData;
---
+++ my-app/src/interface/UserInterface.ts
@@ -0,0 +1,9 @@
+const UserInterface = {
+    id: 'number',
+    name: 'string',
+    username: 'string',
+    email: 'string',
+    role: 'string'
+}
+
+export default UserInterface;
