aac179daa56bef8e21efc81de58a15e866ed6454
Darvan Ceyhan <115955030+darwanceyhan@users.noreply.github.com>
2023-05-04 02:32:03 +0300

    change svg image with png and update popup form

--- my-app/src/components/popup/popup.tsx
+++ my-app/src/components/popup/popup.tsx
@@ -1,4 +1,14 @@
 import CloseIcon from "@mui/icons-material/Close";
+import {
+  Stack,
+  TextField,
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Avatar,
+} from "@mui/material";
 const Popup = (props: any) => {
   return (
     <div className="popup-container">
@@ -11,6 +21,66 @@ const Popup = (props: any) => {
         >
           <CloseIcon />
         </div>
+
+        <Stack spacing={2} sx={{ width: "90%" }} className="mx-auto">
+          <TextField id="outlined-basic" label="Full Name" variant="outlined" />
+          <TextField id="outlined-basic" label="Username" variant="outlined" />
+          <TextField
+            id="outlined-basic"
+            label="Email Adress"
+            variant="outlined"
+          />
+          <Box>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Role</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Age"
+              >
+                <MenuItem value={10}>Contributor</MenuItem>
+                <MenuItem value={20}>Subscriber</MenuItem>
+                <MenuItem value={30}>Author</MenuItem>
+                <MenuItem value={40}>Adminstrator</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Stack>
+
+        <Stack
+          direction="column"
+          spacing={2}
+          sx={{ width: "90%" }}
+          className="mx-auto mt-5"
+        >
+          <h2 className="mt-20">Select Avatar</h2>
+          <Stack direction="row" spacing={2}>
+            <Avatar
+              variant="square"
+              sx={{ height: "50px", width: "50px" }}
+            ></Avatar>
+            <Avatar
+              variant="square"
+              sx={{ height: "50px", width: "50px" }}
+            ></Avatar>
+            <Avatar
+              variant="square"
+              sx={{ height: "50px", width: "50px" }}
+            ></Avatar>
+            <Avatar
+              variant="square"
+              sx={{ height: "50px", width: "50px" }}
+            ></Avatar>
+            <Avatar
+              variant="square"
+              sx={{ height: "50px", width: "50px" }}
+            ></Avatar>
+            <Avatar
+              variant="square"
+              sx={{ height: "50px", width: "50px" }}
+            ></Avatar>
+          </Stack>
+        </Stack>
       </div>
     </div>
   );
