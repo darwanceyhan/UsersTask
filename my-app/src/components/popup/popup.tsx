23438853b87cb34ca2ab43eefa87610896952a2c
Darvan Ceyhan <115955030+darwanceyhan@users.noreply.github.com>
2023-05-04 04:04:40 +0300

    add form element for user form

--- my-app/src/components/popup/popup.tsx
+++ my-app/src/components/popup/popup.tsx
@@ -8,6 +8,7 @@ import {
   MenuItem,
   Select,
   Avatar,
+  Button,
 } from "@mui/material";
 import { useState } from "react";
 import UserInterface from "../../interface/UserInterface";
@@ -46,65 +47,86 @@ const Popup = (props: PopupProps) => {
           <CloseIcon />
         </div>
 
-        <Stack spacing={2} sx={{ width: "90%" }} className="mx-auto">
-          <TextField id="outlined-basic" label="Full Name" variant="outlined" />
-          <TextField id="outlined-basic" label="Username" variant="outlined" />
-          <TextField
-            id="outlined-basic"
-            label="Email Adress"
-            variant="outlined"
-          />
-          <Box>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Role</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                label="Age"
-              >
-                <MenuItem value={10}>Contributor</MenuItem>
-                <MenuItem value={20}>Subscriber</MenuItem>
-                <MenuItem value={30}>Author</MenuItem>
-                <MenuItem value={40}>Adminstrator</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-        </Stack>
-
-        <Stack
-          direction="column"
-          spacing={2}
-          sx={{ width: "90%" }}
-          className="mx-auto mt-5"
-        >
-          <h2 className="mt-20">Select Avatar</h2>
-          <Stack direction="row" spacing={2}>
-            <Avatar
-              variant="square"
-              sx={{ height: "50px", width: "50px" }}
-            ></Avatar>
-            <Avatar
-              variant="square"
-              sx={{ height: "50px", width: "50px" }}
-            ></Avatar>
-            <Avatar
-              variant="square"
-              sx={{ height: "50px", width: "50px" }}
-            ></Avatar>
-            <Avatar
-              variant="square"
-              sx={{ height: "50px", width: "50px" }}
-            ></Avatar>
-            <Avatar
-              variant="square"
-              sx={{ height: "50px", width: "50px" }}
-            ></Avatar>
-            <Avatar
-              variant="square"
-              sx={{ height: "50px", width: "50px" }}
-            ></Avatar>
+        <form onSubmit={addUser}>
+          <Stack spacing={2} sx={{ width: "90%" }} className="mx-auto">
+            <TextField
+              id="outlined-basic"
+              label="Full Name"
+              variant="outlined"
+              value={fullname}
+              onChange={(e) => setFullname(e.target.value)}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Username"
+              variant="outlined"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Email Adress"
+              variant="outlined"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Box>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Role</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  label="Age"
+                  value={role}
+                  onChange={(e) => setRole(e.target.value)}
+                >
+                  <MenuItem value={"Contributor"}>Contributor</MenuItem>
+                  <MenuItem value={"Subscriber"}>Subscriber</MenuItem>
+                  <MenuItem value={"Author"}>Author</MenuItem>
+                  <MenuItem value={"Adminstrator"}>Adminstrator</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
           </Stack>
-        </Stack>
+
+          <Stack
+            direction="column"
+            spacing={2}
+            sx={{ width: "90%" }}
+            className="mx-auto mt-5"
+          >
+            <h2 className="mt-20">Select Avatar</h2>
+            <Stack direction="row" spacing={2}>
+              <Avatar
+                variant="square"
+                sx={{ height: "50px", width: "50px" }}
+              ></Avatar>
+              <Avatar
+                variant="square"
+                sx={{ height: "50px", width: "50px" }}
+              ></Avatar>
+              <Avatar
+                variant="square"
+                sx={{ height: "50px", width: "50px" }}
+              ></Avatar>
+              <Avatar
+                variant="square"
+                sx={{ height: "50px", width: "50px" }}
+              ></Avatar>
+              <Avatar
+                variant="square"
+                sx={{ height: "50px", width: "50px" }}
+              ></Avatar>
+              <Avatar
+                variant="square"
+                sx={{ height: "50px", width: "50px" }}
+              ></Avatar>
+            </Stack>
+          </Stack>
+          <Button variant="contained" className="text-sm" type="submit">
+            Add New User
+          </Button>
+        </form>
       </div>
     </div>
   );
