af6123e212baa91824ba59387d14f97189dde7c2
Darvan Ceyhan <115955030+darwanceyhan@users.noreply.github.com>
2023-05-10 00:12:59 +0300

    change the style

--- my-app/src/components/pagination/pagination.tsx
+++ my-app/src/components/pagination/pagination.tsx
@@ -16,7 +16,7 @@ const Pagination = (props: PaginationProps) => {
       <nav aria-label="Page navigation">
         <ul className="inline-flex">
           <li>
-            <button className="rounded h-10 px-5 text-pagination-text transition-colors duration-150 rounded-l-lg focus:shadow-outline hover:bg-pagination-bg">
+            <button className="rounded h-10 px-5 text-pagination-text transition-colors duration-150 rounded-l-lg focus:shadow-outline hover:bg-pagination-bg hover:opacity-40">
               <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
                 <path
                   d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
@@ -33,7 +33,7 @@ const Pagination = (props: PaginationProps) => {
                   className={
                     props.page === i + 1
                       ? "h-10 px-5 text-pagination-text transition-colors duration-150 bg-pagination-bg rounded focus:shadow-outline hover:bg-pagination-bg rounded"
-                      : "h-10 px-5 text-pagination-text transition-colors duration-150 bg-white rounded focus:shadow-outline hover:bg-pagination-bg rounded"
+                      : "h-10 px-5 text-pagination-text transition-colors duration-150 bg-white rounded focus:shadow-outline hover:bg-pagination-bg rounded hover:opacity-40"
                   }
                   value={i + 1}
                   onClick={ChangePage}
@@ -44,7 +44,7 @@ const Pagination = (props: PaginationProps) => {
             );
           })}
           <li>
-            <button className="h-10 px-5 text-pagination-text transition-colors duration-150 bg-white rounded-r-lg focus:shadow-outline hover:bg-pagination-bg rounded">
+            <button className="h-10 px-5 text-pagination-text transition-colors duration-150 bg-white rounded-r-lg focus:shadow-outline hover:bg-pagination-bg rounded hover:opacity-40">
               <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
                 <path
                   d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
